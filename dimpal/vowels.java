fe98cbf158cd8d7d8acf64987d6e605198e88279
tulsi567 <54350137+tulsi567@users.noreply.github.com>
2019-08-21 13:04:59 +0530

    Add files via upload

---
+++ dimpal/vowels.java
@@ -0,0 +1,30 @@
+import java .io.*;
+{
+public static void main(string[]args)throw exception
+{
+char ch;
+system.out.println("enter char")
+DataInputStream dis=new DataInputStream(system.in);
+ch=dis.readLine();
+switch(ch)
+{
+case'a' : system.out.println("given char is vowel");
+break;
+
+case'e' : system.out.println("given char is vowel")
+break;
+
+case'i' : system.out.println("given char is vowel")
+break;
+
+case'o' : system.out.println("given char is vowel")
+break;
+
+case'u' : system.out.println("given char is vowel")
+break;
+
+default : system.out.println("enter vowel char only");
+break;
+}
+}
+}
